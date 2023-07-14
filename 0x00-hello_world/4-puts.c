e3361017a696e41a8d143c53007db952f65ab283
Joshshegz <adesanwojoshua21@gmail.com>
2023-07-14 03:04:06 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {i
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0); 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
 }
